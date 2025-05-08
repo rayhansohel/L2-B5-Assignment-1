5b029a25967995fbe48a81dd5945963242f63098
RayhanSohel <arayhansohel@gmail.com>
2025-05-08 13:59:42 +0600

    3rd problem solution

--- solutions.js
+++ solutions.js
@@ -3,14 +3,18 @@ function formatString(input, toUpper) {
     return toUpper ? input.toUpperCase() : input.toLowerCase();
 }
 function filterByRating(items) {
-    var result = items.filter(function (item) { return item.rating >= 4; });
+    return items.filter(function (item) { return item.rating >= 4; });
+}
+function concatenateArrays() {
+    var arrays = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        arrays[_i] = arguments[_i];
+    }
+    var result = arrays.reduce(function (a, b) { return a.concat(b); }, []);
     console.log(result);
     return result;
 }
-var books = [
-    { title: "Book A", rating: 4.5 },
-    { title: "Book B", rating: 4.2 },
-    { title: "Book C", rating: 5.0 }
-];
-filterByRating(books);
+concatenateArrays(["a", "b"], ["c"]);
+concatenateArrays([1, 2], [3, 4], [5]);
+concatenateArrays([1, 2], [3, 4, 6, 8], [5]); 
 
--- solutions.ts
+++ solutions.ts
@@ -1,9 +1,14 @@
 
 function formatString(input: string, toUpper: boolean = true): string {
     return toUpper ? input.toUpperCase() : input.toLowerCase();
-  }
+}
 
 
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
     return items.filter(item => item.rating >= 4);
-  }
+}
+
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+    return arrays.reduce((a, b) => a.concat(b), []);
+}
